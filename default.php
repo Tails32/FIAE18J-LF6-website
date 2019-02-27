edaab4f7d118cdcd3b7893b0c0d48e21595e2b9d
Tails32 <tails32@hotmail.de>
2019-02-27 14:33:09 +0100

    Moved code to separate files for header and footer

--- default.php
+++ default.php
@@ -1,33 +1,18 @@
-<!DOCTYPE html>
-<html lang="de">
-	<head>
-		<title>Expo-Plaza Hannover</title>
-		<meta charset="UTF-8">
-		<link rel="icon" href="assets/images/favicon.png" type="image/png">
-		<link rel="stylesheet" type="text/css" href="assets/bootstrap/4.1.3/css/bootstrap.min.css">
-        <script src="assets/jquery/jquery-3.3.1.min.js"></script>
-        <script src="assets/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-		<meta name="theme-color" content="MediumSeaGreen">
-	</head>
-	<body style="background-color:#ffeea8;">
-		<img src ="assets\images\mainheader.jpg" class ="img-responsive">
-		<div class="jumbotron text-center">
-  			<h1>Hannover Expo Gelände</h1>
-  			<p>Messegelände mit lebendiger Weltausstellungsgeschichte.</p> 
+<img src ="assets\images\mainheader.jpg" class ="img-responsive">
+<div class="jumbotron text-center">
+	<h1>Hannover Expo Gelände</h1>
+	<p>Messegelände mit lebendiger Weltausstellungsgeschichte.</p> 
+</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm">
+			One of three columns
 		</div>
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-sm">
-					One of three columns
-				</div>
-				<div class="col-sm">
-					One of three columns
-				</div>
-				<div class="col-sm">
-					One of three columns
-				</div>
-			</div>
+		<div class="col-sm">
+			One of three columns
 		</div>
-	</body>
-</html>
+		<div class="col-sm">
+			One of three columns
+		</div>
+	</div>
+</div>
