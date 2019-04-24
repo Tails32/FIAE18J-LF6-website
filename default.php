33b5d0d6de30f7aa3a7502cc3017986e0c7f9f1b
denmakash <44231587+denmakash@users.noreply.github.com>
2019-04-24 14:01:55 +0200

    bla

--- default.php
+++ default.php
@@ -1,18 +1,46 @@
-<img src ="assets\images\mainheader.jpg" class ="img-responsive">
-<div class="jumbotron text-center">
-	<h1>Hannover Expo Gelände</h1>
-	<p>Messegelände mit lebendiger Weltausstellungsgeschichte.</p> 
-</div>
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-sm">
-			One of three columns
-		</div>
-		<div class="col-sm">
-			One of three columns
-		</div>
-		<div class="col-sm">
-			One of three columns
-		</div>
-	</div>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+   
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+
+    <title>Hannover Expo Gelände</title>
+  </head>
+
+<img src= "assets\images\mainheader.jpg" class ="img-responsive" content = "width=device-width"/>
+
+<body>
+    <div
+      class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm"
+    >
+      <h5 class="my-0 mr-md-auto font-weight-normal"></h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="#">Allgemein</a>
+        <a class="p-2 text-dark" href="#">Orte</a>
+        <a class="p-2 text-dark" href="#">Veranstaltungen</a>
+        <a class="p-2 text-dark" href="#">Impressum</a>
+      </nav>
+      <a class="btn btn-outline-primary" href="#">Sign up</a>
+    </div>
+	
+	<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">Expo Plaza</h1>
+      <p class="lead">
+	  Das Messegelände Hannover befindet sich im Süden der Landeshauptstadt Hannover (Stadtteil Mittelfeld) und ist mit einer überdachten Fläche von 496.000 m² und 26 Messehallen das größte Messegelände der Welt. Zu der 131 Hektar großen Kernfläche gehört außerdem ein Tagungszentrum. Die Betreiberin des Geländes ist die Deutsche Messe AG (DMAG), die auch Messen außerhalb des Geländes veranstaltet.
+
+Im Jahr 2000 wurde auf dem gesamten Messegelände die Expo 2000 ausgerichtet. Auf dem Messegelände werden jährlich bedeutende Messen wie die Agritechnica oder die Hannover Messe ausgerichtet.
+      </p>
+    </div>
+	</body>
